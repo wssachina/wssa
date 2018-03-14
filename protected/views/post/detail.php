1fc42774893185cae6aa5a0cb0fdea7ec9237311
Baiqiang <qiyuuu@gmail.com>
2018-03-14 13:48:08 +0800

    Remove author

--- protected/views/post/detail.php
+++ protected/views/post/detail.php
@@ -8,7 +8,6 @@
         <p class="meta">
           <span class="date"><i class="fa fa-calendar"></i> <?php echo date('Y-m-d', $news->date); ?></span>
           <span class="time"><i class="fa fa-clock-o"></i> <?php echo date('H:i', $news->date); ?></span>
-          <span class="author"><i class="fa fa-user"></i> <?php echo $news->user->getAttributeValue('name', true); ?></span>
         </p>
         <div class="desc"><?php echo $news->getAttributeValue('content'); ?></div>
       </div>
--- protected/views/site/news.php
+++ protected/views/site/news.php
@@ -7,7 +7,6 @@
       <p class="meta">
         <span class="date"><i class="fa fa-calendar"></i> <?php echo date('Y-m-d', $data->date); ?></span>
         <span class="time"><i class="fa fa-clock-o"></i> <?php echo date('H:i', $data->date); ?></span>
-        <span class="author"><i class="fa fa-user"></i> <?php echo $data->user->getAttributeValue('name', true); ?></span>
       </p>
       <div class="desc"><?php echo $data->getDescriptionOrContent(); ?></div>
     </div>
