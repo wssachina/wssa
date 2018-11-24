9bbb7d7882c435cc069f20a0657c042c42c6b9fd
Baiqiang <qiyuuu@gmail.com>
2018-11-25 00:28:26 +0800

    Add invitation code generator

--- protected/models/InvitationCode.php
+++ protected/models/InvitationCode.php
@@ -10,25 +10,40 @@
  * @property string $create_time
  * @property string $update_time
  */
-class InvitationCode extends ActiveRecord
-{
+class InvitationCode extends ActiveRecord {
+
+	protected function beforeValidate() {
+		if ($this->isNewRecord) {
+			$this->generateCode();
+		}
+		return parent::beforeValidate();
+	}
+
+	public function generateCode($length = 6) {
+		$str = '23456789QWERTYUPASDFGHJKLZXCVBNM';
+		$code = '';
+		for ($i = 0; $i < $length; $i++) {
+			$code .= $str{mt_rand(0, strlen($str) - 1)};
+		}
+		$this->code = $code;
+	}
+
 	/**
 	 * @return string the associated database table name
 	 */
-	public function tableName()
-	{
+	public function tableName() {
 		return 'invitation_code';
 	}
 
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
+	public function rules() {
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
 			['code', 'required'],
+			['code', 'unique'],
 			['status', 'numerical', 'integerOnly'=>true],
 			['code', 'length', 'max'=>10],
 			['create_time, update_time', 'length', 'max'=>11],
@@ -41,8 +56,7 @@ class InvitationCode extends ActiveRecord
 	/**
 	 * @return array relational rules.
 	 */
-	public function relations()
-	{
+	public function relations() {
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
@@ -52,8 +66,7 @@ class InvitationCode extends ActiveRecord
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels()
-	{
+	public function attributeLabels() {
 		return array(
 			'id' => 'ID',
 			'code' => 'Code',
@@ -75,17 +88,16 @@ class InvitationCode extends ActiveRecord
 	 * @return CActiveDataProvider the data provider that can return the models
 	 * based on the search/filter conditions.
 	 */
-	public function search()
-	{
+	public function search() {
 		// @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria=new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id,true);
-		$criteria->compare('code',$this->code,true);
-		$criteria->compare('status',$this->status);
-		$criteria->compare('create_time',$this->create_time,true);
-		$criteria->compare('update_time',$this->update_time,true);
+		$criteria->compare('id', $this->id, true);
+		$criteria->compare('code', $this->code, true);
+		$criteria->compare('status', $this->status);
+		$criteria->compare('create_time', $this->create_time, true);
+		$criteria->compare('update_time', $this->update_time, true);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
@@ -98,8 +110,7 @@ class InvitationCode extends ActiveRecord
 	 * @param string $className active record class name.
 	 * @return InvitationCode the static model class
 	 */
-	public static function model($className=__CLASS__)
-	{
+	public static function model($className = __CLASS__) {
 		return parent::model($className);
 	}
 }
