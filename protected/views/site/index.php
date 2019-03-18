7a434f6ef71e541c60b4f76a74577b78969de909
Baiqiang <qiyuuu@gmail.com>
2019-03-18 22:27:40 +0800

    Fix slider dots' position

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,10 +1,12 @@
-<?php $this->widget('Carousel', [
-  'items'=>$sliders,
-  'options'=>[],
-  'htmlOptions'=>[
-    'class'=>'slider-wrapper col-md-12',
-  ],
-]); ?>
+<div class="col-md-12">
+  <?php $this->widget('Carousel', [
+    'items'=>$sliders,
+    'options'=>[],
+    'htmlOptions'=>[
+      'class'=>'slider-wrapper',
+    ],
+  ]); ?>
+</div>
 <?php $this->widget('ListView', array(
   'itemView'=>'news',
   'dataProvider'=>$news->search(),
