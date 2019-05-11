38775f74b441279bb484050b7799a072bb39e670
Baiqiang <qiyuuu@gmail.com>
2019-05-11 23:26:09 +0800

    Remove useless sidebar

--- protected/views/user/side.php
+++ protected/views/user/side.php
@@ -5,28 +5,28 @@
       'label'=>Yii::t('common', 'Profile'),
       'active'=>in_array($this->action->id, ['profile', 'edit']),
     ],
-    [
-      'url'=>['/user/preferredEvents'],
-      'label'=>Yii::t('common', 'Preferred Events'),
-    ],
-    [
-      'url'=>['/user/bind'],
-      'label'=>Yii::t('common', 'Bind Account'),
-    ],
-    [
-      'url'=>['/user/competitions'],
-      'label'=>Yii::t('common', 'My Registration'),
-    ],
-    [
-      'url'=>['/user/competitionHistory'],
-      'label'=>Yii::t('common', 'Competition History'),
-      'visible'=>$this->user->wcaid != '',
-    ],
-    [
-      'url'=>['/user/cert'],
-      'label'=>Yii::t('common', 'My Certificates'),
-      'visible'=>$this->user->hasCerts,
-    ],
+    // [
+    //   'url'=>['/user/preferredEvents'],
+    //   'label'=>Yii::t('common', 'Preferred Events'),
+    // ],
+    // [
+    //   'url'=>['/user/bind'],
+    //   'label'=>Yii::t('common', 'Bind Account'),
+    // ],
+    // [
+    //   'url'=>['/user/competitions'],
+    //   'label'=>Yii::t('common', 'My Registration'),
+    // ],
+    // [
+    //   'url'=>['/user/competitionHistory'],
+    //   'label'=>Yii::t('common', 'Competition History'),
+    //   'visible'=>$this->user->wcaid != '',
+    // ],
+    // [
+    //   'url'=>['/user/cert'],
+    //   'label'=>Yii::t('common', 'My Certificates'),
+    //   'visible'=>$this->user->hasCerts,
+    // ],
   ]; ?>
   <section class="widget hidden-xs">
     <?php $this->widget('zii.widgets.CMenu', array(
